ae593372644ba3f0a931f4ab14d08d21a4fece33
sergiofepinilla <115655254+sergiofepinilla@users.noreply.github.com>
2023-05-25 08:19:22 +0200

    getCookie Duplication Removed

--- home/home.js
+++ home/home.js
@@ -214,18 +214,6 @@ document.addEventListener("DOMContentLoaded", function () {
       document.cookie = name + "=" + (value || "") + expires + "; path=/";
   }
 
-  // Obtener El Valor De Una Cookie
-  function getCookie(name) {
-      var nameEQ = name + "=";
-      var ca = document.cookie.split(";");
-      for (var i = 0; i < ca.length; i++) {
-          var c = ca[i];
-          while (c.charAt(0) == " ") c = c.substring(1, c.length);
-          if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-      }
-      return null;
-  }
-
   // Mostrar notificación de Cookies
   function showCookieNotification() {
       var cookieNotification = document.getElementById("cookie-notification");
